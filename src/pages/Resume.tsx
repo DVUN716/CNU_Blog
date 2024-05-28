fe1cbf8c3bfdd90410831d58f933be3f375d21da
DVUN <dhs9342@naver.com>
2024-05-28 15:50:48 +0900

    굵은 글씨

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,5 +1,10 @@
 const Resume = () => {
-  return <div>나는 프로젝트 내역 왜 안 됌?</div>;
+  return (
+    <div>
+      <strong>나는 프로젝트 내역</strong>
+      <div>왜 안 됌?</div>
+    </div>
+  );
 };
 
 export default Resume;
