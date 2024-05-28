51cc157af0e53f3479c214b6db54168f7f53ce0d
DVUN <dhs9342@naver.com>
2024-05-28 16:23:16 +0900

    내용 시범

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,8 +1,18 @@
 const Resume = () => {
   return (
     <div>
-      <strong>실전 코딩 수업 수강</strong>
+      <h2>
+        <strong>실전 코딩 수업 수강</strong>
+      </h2>
       <p>아무것도 안 나오고</p>
+      <h2>
+        <strong>활동 2</strong>
+      </h2>
+      <p>왜 이렇게 확인해야 하는가</p>
+      <h2>
+        <strong>아이네 포카 빨리 와라</strong>
+      </h2>
+      <p>다이소 탑로더 이러다 그냥 질러 버릴 듯</p>
     </div>
   );
 };
